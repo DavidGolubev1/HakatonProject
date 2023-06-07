90ca0f9fadec0e6645fec510b6480136e3ada117
amitbarha <amitbar131@gmail.com>
2023-06-07 16:58:29 +0300

    home page almost

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet, Link } from 'react-router-dom'
 import { useState } from 'react'
+import Logo from './../assets/Logo.png'
 import './../Style/layout.css'
 function Layout() {
     const [burgerBool, setBurgerBool] = useState(false)
@@ -9,12 +10,12 @@ function Layout() {
             <div className="nav-include-all">
                 <div className="navbar">
                     <div className="links-bar">
-                        <Link to={`/`} className="logo-background"><img id="logo" src="" alt="" /></Link>
+                        <Link to={`/`} className="logo-background"><img id="logo" src={Logo} alt="" /></Link>
                         <Link className='nav-links' to={'Voulnteerings'}><div>Serve Finder</div> </Link>
                         <Link className='nav-links' to={'Login'}>Log In </Link>
                         <Link className='nav-links' to={'AddVolunteering'}>Add + </Link>
-                        <Link className='nav-links' to={'Login'}> </Link>
-                        
+                        <Link className='nav-links' to={'Login'}>My Profile</Link>
+
                         <label className="burger" htmlFor="burger">
                             <input type="checkbox" id="burger" onChange={() => setBurgerBool(!burgerBool)} />
                             <span></span>
@@ -31,7 +32,26 @@ function Layout() {
             </div>
             <Outlet />
             <div className="footer">
-                footer
+                <div className="website-name-footer">
+                    Help Hub
+                </div>
+                <div className="social-links">
+                    <a href="https://www.facebook.com/amitcapi" target="_blank" rel="noopener noreferrer" >
+                        <img width="30" height="30" src="https://img.icons8.com/color/48/facebook-new.png" alt="facebook-new" />
+                    </a>
+                    <a href="https://github.com/amitbarha" target="_blank" rel="noopener noreferrer" >
+                        <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/github.png" alt="github" />
+                    </a>
+                    <a href="https://www.google.com/maps/place/%D7%94%D7%97%D7%99%D7%9C%D7%96%D7%95%D7%9F+3,+%D7%A8%D7%9E%D7%AA+%D7%92%D7%9F%E2%80%AD/@32.0872801,34.8040903,17z/data=!3m1!4b1!4m6!3m5!1s0x151d4bc6f14be6b9:0xccb089b29dfbffe2!8m2!3d32.0872801!4d34.8040903!16s%2Fg%2F11fx117dcw" target="_blank" rel="noopener noreferrer" >
+                        <img width="30" height="30" src="https://img.icons8.com/color/48/google-maps-new.png" alt="google-maps-new" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/amit-barhanin-588852262/" target="_blank" rel="noopener noreferrer" >
+                        <img width="30" height="30" src="https://img.icons8.com/fluency/48/linkedin.png" alt="linkedin" />
+                    </a>
+                </div>
+                <div className="made-by">
+                    made by The best team
+                </div>
             </div>
 
 
